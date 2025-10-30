9b46c853e34df713926cd80bfc5dece78e051a88
Will M <114441559+wmarchi1@users.noreply.github.com>
2025-10-29 21:19:18 -0400

    Update chip_tb.vhd

--- Working Versions/iterationFour/chip_tb.vhd
+++ Working Versions/iterationFour/chip_tb.vhd
@@ -1,3 +1,7 @@
+--exec xvhdl dff_neg.vhd xor2.vhd program_counter.vhd cache_fsm.vhd peripheral_interface2.vhd cache_controller_top.vhd chip.vhd chip_tb.vhd
+--exec xelab chip_tb -debug typical -s sim_out
+--exec xsim sim_out -gui
+
 library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
